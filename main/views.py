abbb59a39a41838cf30a0cf3a082078302c18656
madina myrzakulova <madina.japarbekovna@jmail.com>
2021-08-17 00:08:58 +0300

    added second test age and url

--- main/views.py
+++ main/views.py
@@ -6,6 +6,9 @@ def homepage(request):
     return HttpResponse("Hello world")
 
 def test(request):
-    return render (request, "test.html")    
+    return render (request, "test.html")  
+
+def second(request):
+    return HttpResponse("test 2 page")    
 
 
